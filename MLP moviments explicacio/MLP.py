a25834ad0e2b11108c5fcd23d143f5904583da78
sergial273 <sergial273@gmail.com>
2023-05-13 19:26:18 +0200

    updated explanation of moves

--- MLP moviments explicacio/MLP.py
+++ MLP moviments explicacio/MLP.py
@@ -5,10 +5,109 @@ from keras.utils import to_categorical
 from keras.models import Sequential
 from keras.layers import InputLayer
 from keras.layers import Dense
-from keras.layers import Dropout
+
+def definirmoviment(fen1, fen2, moved):
+    PiecesTonNum = {
+    "p": "1",
+    "n": "2",
+    "b": "3",
+    "r": "4",
+    "q": "5",
+    "k": "6"
+    }
+
+    center = [27, 28, 35, 36]
+    outside = [0, 1, 2, 3, 4, 5, 6, 7, 8, 15, 16, 23, 24, 31, 32, 39, 40, 47, 48, 55, 56, 57, 58, 59, 60, 61, 62, 63]
+
+    board1 = chess.Board(fen1)
+    board2 = chess.Board(fen2)
+
+    fen1 = fen1.split()
+    fen2 = fen2.split()
+
+    #check if there has been a capture
+    a = b = 0
+    codifsencera1 = codifsencera2 = ""
+    for elem in fen1[0]:
+        if not elem.isdigit() and elem != "/":
+            a += 1
+            codifsencera1 += elem
+        elif elem == "/":
+            codifsencera1 += elem
+        else:
+            codifsencera1 += ("1" * int(elem))
+        
+
+    for elem in fen2[0]:
+        if not elem.isdigit() and elem != "/":
+            b += 1
+            codifsencera2 += elem
+        elif elem == "/":
+            codifsencera2 += elem
+        else:
+            codifsencera2 += ("1" * int(elem))
+    
+
+    col = 0
+    fil = 0
+    count = 0
+    position1 = 0
+    #checking which piece moved
+    for elem in codifsencera2:
+
+        if elem.isdigit() and elem != codifsencera1[count] and codifsencera1[count].isascii():
+            if moved == PiecesTonNum[codifsencera1[count].lower()]:   
+                piece = codifsencera1[count]
+                position1 = (7-fil)*8 + col
+            
+        if elem == "/" and position1 == 0:
+            fil += 1
+            col = -1
+    
+        col += 1
+        count += 1
+
+    col = 0
+    fil = 0
+    count = 0
+    position2 = 0
+    #checking which piece moved
+    for elem in codifsencera1:
+        if elem.isdigit() and elem != codifsencera2[count] and codifsencera2[count].isascii():
+            if moved == PiecesTonNum[codifsencera2[count].lower()]:   
+                piece = codifsencera2[count]
+                position2 = (7-fil)*8 + col
+            
+        if elem == "/" and position2 == 0:
+            fil += 1
+            col = -1
+
+        col += 1
+        count += 1
+
+    centre = 0
+    mig = 0
+    fora = 0
+    if position2 in center:
+        centre = 1
+    elif position2 in outside:
+        fora = 1
+    else:
+        mig = 1
 
 
-def eval6(eval,mate):
+    control = 0
+    if len(board2.attacks(position2)) - len(board1.attacks(position1)) < 0:
+        #perd d'algunes caselles
+        control = 1
+
+    captura = a-b #És captura
+    check = 1 if board1.is_check() else 0 #Està en jaque
+
+    return captura, check, centre, mig, fora, control
+
+
+def eval6(eval,mate, fen1, fen2, moved):
     output_bin = []
 
     #calcculating evaluation ranges
@@ -56,6 +155,14 @@ def eval6(eval,mate):
 
     #adding the mate bit
     output_bin.append(mate)
+
+    captura, escac, centre, mig, exterior, control = definirmoviment(fen1, fen2, moved)
+    output_bin.append(captura)
+    output_bin.append(escac)
+    output_bin.append(centre)
+    output_bin.append(mig)
+    output_bin.append(exterior)
+    output_bin.append(control)
 
     #checking if piece attacks new ones
     arr1 = np.array(output_bin, dtype=float)
@@ -135,19 +242,6 @@ def xarxa7():
     MLP.add(Dense(func[1], activation='sigmoid')) # output layer
     return MLP
 
-def xarxa2Dropout():
-    MLP = Sequential()
-    MLP.add(InputLayer(input_shape=(128, ))) # input layer
-    MLP.add(Dense(256, activation='sigmoid')) # hidden layer 1
-    MLP.add(Dropout(0.45)) # hidden layer 1
-    MLP.add(Dense(64, activation='sigmoid')) # hidden layer 1
-    MLP.add(Dropout(0.45)) # hidden layer 1
-    #MLP.add(Dense(64, activation='sigmoid')) # hidden layer 2
-    #MLP.add(Dense(32, activation='sigmoid')) # hidden layer 2
-    #MLP.add(Dense(16, activation='sigmoid')) # hidden layer 2
-    MLP.add(Dense(func[1], activation='sigmoid')) # output layer
-    return MLP
-
 def getTuples(numEvaluacions,numTests):
     # Abrir el archivo csv y leer los primeros 'numEvaluacions' valores
     with open(os.getcwd()+'\MLP moviments\PosicionsEvaluacions2.csv', 'r') as file:
@@ -212,7 +306,7 @@ def convertTuple(Tuples, func):
 
         eval = int(line[2])/1000
 
-        arr1 = func(eval,line[4])
+        arr1 = func(eval,line[4],line[0],line[1],line[3])
         
         outputs.append(arr1)
 
@@ -222,7 +316,7 @@ def convertTuple(Tuples, func):
     return inputs,outputs
 
 
-evalutionFunctions = [(eval6,21)]
+evalutionFunctions = [(eval6,27)]
 differentNetworks = [xarxa2] #[xarxa1,xarxa2,xarxa3,xarxa4,xarxa5,xarxa6,xarxa7,xarxa2Dropout]
 listOptimizers = ['Adam'] #['SGD','RMSprop','Adam','Adadelta','Adagrad','Adamax','Nadam','Ftrl']
 
--- test.py
+++ test.py
@@ -12,19 +12,16 @@ PiecesTonNum = {
 center = [27, 28, 35, 36]
 outside = [0, 1, 2, 3, 4, 5, 6, 7, 8, 15, 16, 23, 24, 31, 32, 39, 40, 47, 48, 55, 56, 57, 58, 59, 60, 61, 62, 63]
 
-moved = "1"
+moved = "2"
 
-fen1 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-fen2 = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
+fen1 = "rnbqk1nr/pp1p1pbp/4p1p1/2p5/7P/1P3P2/PBPPPNP1/R2QKBNR w KQkq - 0 1"#"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+fen2 = "rnbqk1nr/pp1p1pbp/4p1p1/2p5/4N2P/1P3P2/PBPPP1P1/R2QKBNR w KQkq - 0 1"#"rnbqkbnr/pppppppp/8/8/8/1P6/P1PPPPPP/RNBQKBNR w KQkq - 0 1"
 
 board1 = chess.Board(fen1)
-
-
-
+board2 = chess.Board(fen2)
 
 fen1 = fen1.split()
 fen2 = fen2.split()
-
 
 #check if there has been a capture
 a = b = 0
@@ -49,40 +46,43 @@ for elem in fen2[0]:
         codifsencera2 += ("1" * int(elem))
         
     
-    
+col = 0
+fil = 0
 count = 0
-bars = 0
 position1 = 0
 #checking which piece moved
 for elem in codifsencera2:
+
     if elem.isdigit() and elem != codifsencera1[count] and codifsencera1[count].isascii():
         if moved == PiecesTonNum[codifsencera1[count].lower()]:   
             piece = codifsencera1[count]
-            position1 = count
+            position1 = (7-fil)*8 + col
         
     if elem == "/" and position1 == 0:
-        bars += 1
-
+        fil += 1
+        col = -1
+   
+    col += 1
     count += 1
 
-position1 = 64 - (position1-bars) - 1#position of the piece in board1 that has been moved
 
+col = 0
+fil = 0
 count = 0
-bars = 0
 position2 = 0
 #checking which piece moved
 for elem in codifsencera1:
     if elem.isdigit() and elem != codifsencera2[count] and codifsencera2[count].isascii():
         if moved == PiecesTonNum[codifsencera2[count].lower()]:   
             piece = codifsencera2[count]
-            position2 = count 
+            position2 = (7-fil)*8 + col
         
     if elem == "/" and position2 == 0:
-        bars += 1
+        fil += 1
+        col = -1
 
+    col += 1
     count += 1
-
-position2 = 64 - (position2-bars) - 1 #position of the piece in board2 that has been moved
 
 
 centre = 0
@@ -104,6 +104,16 @@ print (check)
 print (codifsencera1)
 print (codifsencera2)
 
+print(position1)
 print(position2)
 print(piece)
 
+
+print(len(board2.attacks(position2)))
+
+print(len(board1.attacks(position1)))
+
+control = 0
+if len(board2.attacks(position2)) - len(board1.attacks(position1)) < 0:
+    #perd d'algunes caselles
+    control = 1
