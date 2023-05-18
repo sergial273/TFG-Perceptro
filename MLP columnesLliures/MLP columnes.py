530bcfa733ff157bbe1ca623c2c701a6a5ad6e83
sergial273 <sergial273@gmail.com>
2023-05-18 23:17:18 +0200

    Update MLP columnes.py

--- MLP columnesLliures/MLP columnes.py
+++ MLP columnesLliures/MLP columnes.py
@@ -260,10 +260,23 @@ for func in evalutionFunctions:
                                             batch_size=128,
                                             verbose=0)
 
-            with open(os.getcwd()+'\MLP columnesLliures\\test.txt', mode='a') as archivo:
+            """with open(os.getcwd()+'\MLP columnesLliures\\test.txt', mode='a') as archivo:
                 archivo.write('Xarxa, Funcio eval, Optimitzador: '+str(xarxa)+', '+str(func)+', '+str(optimizer)+'\n')
                 archivo.write('Train acc '+str(train_accuracy)+'\n')
                 archivo.write('Test acc '+str(test_acc)+'\n')
-                archivo.write("-" * 50+'\n')
+                archivo.write("-" * 50+'\n')"""
+            
+
+            TrainingTuples,TestTuples = getTuples(numEvaluacions=5,numTests=5)
+
+
+            inputsTraining,outputsTraining = convertTuple(TrainingTuples, aCasellaLliure)
+
+
+            inputsTest,outputsTest = convertTuple(TestTuples, aCasellaLliure)
+
+            print("AAAAAAAAA")
+            print(MLP.predict(inputsTest))
+
             
 
